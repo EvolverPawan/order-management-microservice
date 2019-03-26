d814572ec1ad20d9f3e17fd65a94b96299bb42cd
pawan <evolver.pawan@gmail.com>
2019-03-26 06:51:57 +0530

    Added models

---
+++ order-service/src/models/index.spec.js
@@ -0,0 +1,33 @@
+/* eslint-env mocha */
+const test = require('assert')
+const { validate } = require('./')
+
+console.log(Object.getPrototypeOf(validate))
+
+describe('Schemas Validation', () => {
+  it('can validate a user object', (done) => {
+    const testUser = {
+      name: 'Cristian',
+      lastName: 'Ramirez',
+      email: 'cristiano@nupp.com',
+      creditCard: {
+        number: '1111222233334444',
+        cvc: '123',
+        exp_month: '07',
+        exp_year: '2017'
+      },
+      membership: '7777888899990000'
+    }
+
+    validate(testUser, 'user')
+      .then(value => {
+        console.log('validated')
+        console.log(value)
+        done()
+      })
+      .catch(err => {
+        console.log(err)
+        done()
+      })
+  })
+})
